64c19b8edaa90e1f0bd2d5e6a69d511119a39b25
Dzung Luong <164067302+ltdungg@users.noreply.github.com>
2024-07-09 22:51:44 +0700

    Update settings.py

--- TikiCrawler/TikiCrawler/settings.py
+++ TikiCrawler/TikiCrawler/settings.py
@@ -91,5 +91,5 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
-CHROME_DRIVER_PATH = 'D:\crawl-data-from-tiki\TikiCrawler\chromedriver.exe'
+CHROME_DRIVER_PATH = '..\chromedriver.exe'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
